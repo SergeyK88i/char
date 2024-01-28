c65bba70b0d6200023248ac36c9aaf7e868131c7
S <com.com>
2024-01-28 19:25:31 +0300

    edit users

--- src/app/users.ts
+++ src/app/users.ts
@@ -2,5 +2,13 @@ export const users = [
     {
         email: "!@123",
         password: "qwe123"
+    },
+    {
+        email: "!@admin",
+        password: "psikod2"
+    },
+    {
+        email: "!@PO",
+        password: "psikod2"
     }
 ]
