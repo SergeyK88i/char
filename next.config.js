22e2829399bab1e05d5f19c7dba23d62f3d94d64
S <com.com>
2024-01-28 15:28:57 +0300

    netx.config.js: - output: "standalone"

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export,"
+    output: "standalone",
+    // output:"export",
+    distDir: 'out',
 };
 
 module.exports = nextConfig
